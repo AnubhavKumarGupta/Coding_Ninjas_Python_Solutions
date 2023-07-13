647d92356b64209dc57c512a57aa5ffa44d6fb61
Anubhav Kumar Gupta <122034618+AnubhavKumarGupta@users.noreply.github.com>
2023-07-13 12:13:47 +0530

    Create Square Pattern.py

---
+++ Square Pattern.py
@@ -0,0 +1,12 @@
+## Read input as specified in the question
+## Print the required output in given format
+n=int(input())
+a=n
+i=1
+while i<=n: #for no. of rows
+    j=1
+    while j<=n:
+        print(int(n),end="")
+        j=j+1
+    print()
+    i=i+1
