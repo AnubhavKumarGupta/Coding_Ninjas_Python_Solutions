d96c0d0d900fa31bba483bf9d75aa4f4d5229dfc
Anubhav Kumar Gupta <122034618+AnubhavKumarGupta@users.noreply.github.com>
2023-07-13 12:24:57 +0530

    Create Binary Pattern.py

---
+++ Binary Pattern.py
@@ -0,0 +1,10 @@
+## Read input as specified in the question.
+## Print output as specified in the question.
+n=int(input())
+for i in range(1,n+1):
+    for j in range (1,n-i+2):
+        if ((i%2)==1):
+            print("1",end="")
+        else:
+            print("0",end="")
+    print()
