a2840bcf78ba484be4b8cd9c8b5f19ef326cab6b
Anubhav Kumar Gupta <122034618+AnubhavKumarGupta@users.noreply.github.com>
2023-07-13 12:07:59 +0530

    Create Check number.py

---
+++ Check number.py
@@ -0,0 +1,9 @@
+# Read input as sepcified in the question
+# Print output as specified in the question
+a=int(input())
+if a>0:
+    print("Positive")
+elif a==0:
+    print("Zero")
+elif a<0:
+    print("Negative")
