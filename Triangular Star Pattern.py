518ee0fca5b386f7ad2fdb4fa2c5c912b3eafece
Anubhav Kumar Gupta <122034618+AnubhavKumarGupta@users.noreply.github.com>
2023-07-13 12:14:17 +0530

    Create Triangular Star Pattern.py

---
+++ Triangular Star Pattern.py
@@ -0,0 +1,11 @@
+## Read input as specified in the question
+## Print the required output in given format
+a=int (input())
+i=1
+while i<=a:
+    j=1
+    while j<=i:
+        print("*",end="")
+        j=j+1
+    i=i+1
+    print()
